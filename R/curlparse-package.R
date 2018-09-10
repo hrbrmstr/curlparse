be47179a5f252ec0069e97d550c81bd202b4fdbf
hrbrmstr <bob@rud.is>
2018-09-10 09:49:23 -0400

    R package repo initialization complete

---
+++ R/curlparse-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/curlparse>
+#' - BugReports: <https://gitlab.com/hrbrmstr/curlparse/issues>
+#' 
+#' @md
+#' @name curlparse
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
